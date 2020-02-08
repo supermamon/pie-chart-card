d280bec47f22620a2b5dcb8038337cd84a3d25b5
sdelliot <sdelliot@users.noreply.github.com>
2020-02-08 07:16:03 -0700

    Update pie-chart-card.js
    Changing the tooltips to percentages rather than raw numbers

--- pie-chart-card.js
+++ pie-chart-card.js
@@ -84,7 +84,25 @@ class PieChartCard extends HTMLElement {
                 display: true
              },
             hover: { mode: 'index' },
-            plugins: {colorschemes: { scheme: 'brewer.DarkTwo8' } }
+            plugins: {colorschemes: { scheme: 'brewer.DarkTwo8' } },
+            tooltips: {
+              callbacks: {
+                label: function(tooltipItem, data) {
+                  //get the concerned dataset
+                  var dataset = data.datasets[tooltipItem.datasetIndex];
+                  //calculate the total of this data set
+                  var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                    return previousValue + currentValue;
+                  });
+                  //get the current items value
+                  var currentValue = dataset.data[tooltipItem.index];
+                  //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+                  var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+
+                  return entityNames[tooltipItem.index] + ": " + percentage + "%";
+                }
+              }
+            }
         }
     });
 
